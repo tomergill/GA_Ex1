7217414137b2764b1f4fa6f5b4642749109865dd
Tomer Gill <coolran103@gmail.com>
2018-06-07 20:23:57 +0300

    Initial commit

---
+++ backprop.py
@@ -0,0 +1,124 @@
+from chromosome import *
+import numpy as np
+from time import time
+from sys import argv
+
+g2dg = {sigmoid: lambda x: sigmoid(x) * (1- sigmoid(x)),
+        relu: lambda x: (x > 0).astype(np.int),
+        tanh: lambda x: 1 - np.square(tanh(x))}
+
+
+def neglogloss(y_hat, y):
+    return -np.log(y_hat[y])
+
+
+def backprop(ch, x, y):
+    """
+
+    :param ch:
+    :type ch: Chromosome
+    :return: loss and gradients (list of (gW, gb)s)
+    """
+    y_hat, zs = ch.forward(x)
+    pred = np.argmax(y_hat)
+    loss = neglogloss(y_hat, y)
+
+    grads = []
+    g = ch.activation_func
+    dg = g2dg[g]
+
+    # last layer
+    W, b = ch.layers[-1]
+    dL_dzn = y_hat.copy()
+    dL_dzn[y] -= 1
+    dL_dbn = dL_dzn
+    dL_dWn = np.outer(dL_dzn, g(zs[-2]))
+    grads.append((dL_dWn, dL_dbn))
+
+    # rest of layers
+    prev_W = W
+    dL_dzi = dL_dzn
+    most_layers = ch.layers[:-1]
+    for i, (W, b) in enumerate(most_layers[::-1]):
+        i = ch.n - 1 - i
+        z = zs[i]
+        dL_dzi = dL_dzi.dot(prev_W) * dg(z)
+        gb = dL_dzi
+        gW = np.outer(dL_dzi, g(zs[i-1]))
+        grads.append((gW, gb))
+        prev_W = W
+
+    return pred, loss, reversed(grads)
+
+
+def sgd_update(ch, grads, lr):
+    for layer, layer_grads in zip(ch.layers, grads):
+        for weight, grad in zip(layer, layer_grads):
+            weight -= lr * grad
+
+
+def accuracy_and_loss_on(ch, images, labels, return_pred=False):
+    if return_pred:
+        preds = []
+    total_loss = good = 0.0
+    for x, y in zip(images, labels):
+        y_hat, _ = ch.forward(x)
+        pred = np.argmax(y_hat)
+        good += pred == int(y)
+        total_loss += neglogloss(y_hat, y)
+        if return_pred:
+            preds.append(pred)
+    if return_pred:
+        return good / len(images), total_loss / len(images), preds
+    return good / len(images), total_loss / len(images)
+
+def train_on(ch, train_images, train_labels, dev_images, dev_labels, epochs=15, lr=0.001):
+    print "+-------+------------+-----------+----------+---------+------------+"
+    print "| epoch | train loss | train acc | dev loss | dev acc | epoch time |"
+    print "+-------+------------+-----------+----------+----------------------+"
+
+    for i in xrange(epochs):
+        start = time()
+        total_loss = good = 0.0
+        for x, y in zip(train_images, train_labels):
+            pred, loss, grads = backprop(ch, x, y)
+            good += pred == y
+            total_loss += loss
+            sgd_update(ch, grads, lr)
+        train_acc, train_loss = good / len(train_images), total_loss / len(train_images)
+        dev_acc, dev_loss = accuracy_and_loss_on(ch, dev_images, dev_labels)
+
+        print "| {:^5} | {:010f} | {:8.4f}% | {:7f} | {:6.3f}% | {:08f}s |".format(
+            i, train_loss, train_acc * 100.0, dev_loss, dev_acc * 100.0, time() - start)
+    print "+-------+------------+-----------+----------+---------+------------+\n"
+
+
+def main(sizes):
+    lr = 0.01
+
+    # load data
+    dataloader = MNIST(return_type="numpy")
+    train_images, train_labels = dataloader.load_training()
+    test_images, test_labels = dataloader.load_testing()
+
+    dev_indices = np.random.choice(len(train_images), int(0.2 * len(train_images)))
+    train_indices = list(set(range(len(train_images))) - set(dev_indices))
+    dev_images, dev_labels = train_images[dev_indices], train_labels[dev_indices]
+    train_images, train_labels = train_images[train_indices], train_labels[train_indices]
+
+    # create net, train and test
+    net = Chromosome(sizes, sigmoid)
+    train_on(net, train_images, train_labels, dev_images, dev_labels, lr=lr)
+    test_acc, test_loss, preds = accuracy_and_loss_on(net, test_images, test_labels, True)
+    print "\nTest accuracy {}%, with loss of {}".format(test_acc * 100, test_loss)
+
+    with open("bp_test.pred", "w") as f:
+        f.writelines(map(lambda x: str(int(x)) + "\n", preds))
+
+
+if __name__ == '__main__':
+    if len(argv) == 1:
+        sizes = [784, 200, 10]
+    else:
+        sizes = [784] + map(int, argv[1:]) + [10]
+    main(sizes)
---
+++ chromosome.py
@@ -0,0 +1,52 @@
+import numpy as np
+from mnist import MNIST
+
+
+class Chromosome:
+    def __init__(self, sizes, activ_func):
+        self.layers = []
+        for s1, s2 in zip(sizes[:-1], sizes[1:]):
+            W = initialize_weight(s1, s2)
+            b = initialize_weight(s2)
+            self.layers.append((W, b))
+        self.activation_func = activ_func
+        self.n = len(self.layers)
+
+    def forward(self, x):
+        zs = [x]
+        g = self.activation_func
+        h = x
+        for W, b in self.layers[:-1]:
+            z = W.dot(h) + b
+            zs.append(z)
+            h = g(z)
+
+        # last layer
+        W, b = self.layers[-1]
+        z = W.dot(h) + b
+        zs.append(z)
+        return softmax(z), zs
+
+
+def initialize_weight(s1, s2=None):
+    eps = np.sqrt(6.0 / (s1 + (s2 if s2 is not None else 1)))
+    if s2 is None:
+        return np.random.uniform(-eps, eps, s1)
+    return np.random.uniform(-eps, eps, (s2, s1))
+
+
+def softmax(x):
+    exp = np.exp(x - x.max())
+    return exp / exp.sum()
+
+
+
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+
+def relu(x):
+    return np.maximum(0, x)
+
+
+tanh = np.tanh
